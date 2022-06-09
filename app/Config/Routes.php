98d0b73da1fd77c669f928a5d74d6d79c6bf2840
Retno Ekayanti <80891391+reykayanti@users.noreply.github.com>
2022-06-09 11:09:00 +0700

    update routes

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -21,7 +21,7 @@ $routes->setDefaultController('Home');
 $routes->setDefaultMethod('index');
 $routes->setTranslateURIDashes(false);
 $routes->set404Override();
-$routes->setAutoRoute(true);
+// $routes->setAutoRoute(true);
 // The Auto Routing (Legacy) is very dangerous. It is easy to create vulnerable apps
 // where controller filters or CSRF protection are bypassed.
 // If you don't want to define all routes, please use the Auto Routing (Improved).
@@ -36,8 +36,8 @@ $routes->setAutoRoute(true);
 
 // We get a performance increase by specifying the default
 // route since we don't have to scan directories.
-$routes->get('/', 'Home::index');
-// $routes->get('/test', 'Home::test');
+$routes->get('/', 'C_Home::index');
+$routes->get('/admin', 'C_Dashboard::menu');
 
 /*
  * --------------------------------------------------------------------
